cfdae511bbd3b23288474eb854e61a9fbfcbfe7f
coonom2357 <coonom2357@gmail.com>
2025-10-29 12:24:09 -0700

    speed

--- sentiment_analysis_DP.py
+++ sentiment_analysis_DP.py
@@ -5,27 +5,36 @@ from PIL import Image
 import os
 from itertools import islice
 from sklearn.preprocessing import MinMaxScaler
+from tqdm import tqdm
 
 path = kagglehub.dataset_download("fahadullaha/facial-emotion-recognition-dataset") #download dataset and set the file path as path
 scaler = MinMaxScaler() #set scaler so it squishes pixel values between 0 and 1
 
 #initialize empty dataframe and series to hold data
-mainframe = pd.DataFrame()
-mainseries = pd.Series()
+mainlist = []
+mainseries = []
+base_path = f"{path}/processed_data/" #progress bar
+folders = os.listdir(base_path)
+total_images = sum(len(os.listdir(os.path.join(base_path, folder))) for folder in folders) # progress bar
 
 #loop through each folder (emotion category) and each image file within those folders
-for folder in os.listdir(f"{path}/processed_data/"): 
-    for filename in os.listdir(f"{path}/processed_data/{folder}/"):
-        img = Image.open(f"{path}/processed_data/{folder}/{filename}") #open image
-        nimg_grayscale = img.convert("L") #convert to grayscale
-        img_array = np.asarray(nimg_grayscale) #convert to numpy array
-        timg = scaler.fit_transform(nimg_grayscale) #squash values to fit between 0 and 1
-        arr = timg.flatten() #convert 96x96 array into 1D array of 9216 elements
-        df = pd.DataFrame([arr]) #convert to dataframe
-        mainframe = pd.concat([mainframe, df]) #add to main dataframe
-        mainseries = pd.concat([mainseries, pd.Series([folder])]) #add corresponding label to main series
+with tqdm(total=total_images, desc="Processing images", unit="img") as pbar:
+    for folder in folders: 
+        for filename in islice(os.listdir(f"{path}/processed_data/{folder}/"), None):
+            img = Image.open(f"{path}/processed_data/{folder}/{filename}") #open image
+            nimg_grayscale = img.convert("L") #convert to grayscale
+            img_array = np.asarray(nimg_grayscale) #convert to numpy array
+            timg = img_array/255 #squash values to fit between 0 and 1
+            arr = timg.flatten() #convert 96x96 array into 1D array of 9216 elements
+            mainlist.append(arr)
+            mainseries.append(folder) #add corresponding label to main series
+            pbar.update(1)  # update progress bar
         
 #save dataframe and series as csv files
+mainframe = pd.DataFrame(mainlist)
 mainframe.to_csv('mainframe.csv', index=False)
+mainseries = pd.Series(mainseries)
 mainseries.to_csv('mainseries.csv', index=False)
+print(mainframe.shape, mainframe.size)
+print(mainseries.shape, mainseries.size)
 print("finished")
